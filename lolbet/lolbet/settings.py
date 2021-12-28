d1a6a4be07e5b71ac8d1c39499974502e5e91330
dovahkinnn <sherlockhakan72@gmail.com>
2021-12-28 17:33:47 +0300

    Update settings.py

--- lolbet/lolbet/settings.py
+++ lolbet/lolbet/settings.py
@@ -116,13 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static')
+    
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
